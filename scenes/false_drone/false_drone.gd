d1e916f228858bbffcd96cfc959603209bf521e6
NikitaCherskov <nikitacherskov@gmail.com>
2024-12-18 00:21:25 +0300

    Оптимизировал графику

--- scenes/false_drone/false_drone.gd
+++ scenes/false_drone/false_drone.gd
@@ -6,6 +6,6 @@ func _process(delta):
 	velocity.z = rotation.x
 	velocity.x = rotation.z
 	var rot = Vector3(0.0, 1.0, 0.0)
-	velocity.rotated(rot, rotation.y)
+	velocity = velocity.rotated(rot, rotation.y)
 	position += velocity * delta * SPEED
 	pass
