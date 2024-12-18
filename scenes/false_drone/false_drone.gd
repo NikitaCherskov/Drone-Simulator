c11c3e5e715206cf1154989d03a403df03f055c3
NikitaCherskov <nikitacherskov@gmail.com>
2024-12-18 15:08:12 +0300

    Исправил плавность полета бота

--- scenes/false_drone/false_drone.gd
+++ scenes/false_drone/false_drone.gd
@@ -2,10 +2,9 @@ extends Node3D
 var velocity = Vector3(0.0, 0.0, 0.0)
 var SPEED = 40.0
 
-func _process(delta):
+func _physics_process(delta):
 	velocity.z = rotation.x
 	velocity.x = rotation.z
 	var rot = Vector3(0.0, 1.0, 0.0)
 	velocity = velocity.rotated(rot, rotation.y)
 	position += velocity * delta * SPEED
-	pass
